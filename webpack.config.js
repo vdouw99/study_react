4c79d39ce1af6d04255217477d90070d47b2406d
张三丰 <123456@qq.com>
2017-05-04 18:46:52 +0800

    webpack2.0不好用，换到webpack1.0吧。。

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,3 @@
-/**
- * Created by sf on 2017/5/4.
- */
-
-
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -19,8 +14,8 @@ module.exports = {
         filename: "bundle.js"
     },
 
-    resolve: {
-        extensions: ['', '.js', '.jsx']
+    resolve:{
+        extensions:['', '.js','.jsx']
     },
 
     module: {
@@ -29,11 +24,11 @@ module.exports = {
         //     {test: /\.(js|jsx)$/, loader: "eslint-loader", exclude: /node_modules/}
         // ],
         loaders: [
-            {test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel'},
-            {test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less'},
-            {test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss'},
-            {test: /\.(png|gif|jpg|jpeg|bmp)$/i, loader: 'url-loader?limit=5000'},  // 限制大小5kb
-            {test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader: 'url-loader?limit=5000'} // 限制大小小于5k
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
+            { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
+            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000' },  // 限制大小5kb
+            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000'} // 限制大小小于5k
         ]
     },
 
@@ -56,24 +51,16 @@ module.exports = {
 
         // 打开浏览器
         new OpenBrowserPlugin({
-            url: 'http://localhost:8080'
+          url: 'http://localhost:8080'
         }),
 
         // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
         new webpack.DefinePlugin({
-            __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+          __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
         })
     ],
 
     devServer: {
-        proxy: {
-            // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
-            // koa 代码在 ./mock 目录中，启动命令为 npm run mock
-            '/api': {
-                target: 'http://localhost:3000',
-                secure: false
-            }
-        },
         contentBase: "./public", //本地服务器所加载的页面所在的目录
         colors: true, //终端中输出结果为彩色
         historyApiFallback: true, //不跳转
