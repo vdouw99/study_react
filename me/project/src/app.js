c4b678e1e15fcc6edf2e0229d5433f6b371747cc
zhangsanfeng <123456@qq.com>
2017-06-28 12:36:23 +0800

    试了N多种方法，CSS还是无法从JS中分离出来，先跳过吧

--- me/project/src/app.js
+++ me/project/src/app.js
@@ -1,9 +1,9 @@
-// import Layer from './components/layer/layer.js';
+import Layer from './components/layer/layer.js';
 import './style/common.css';
 
 const App = function () {
     var dom = document.getElementById("app");
-    // var layer = new Layer();
+    var layer = new Layer();
     dom.innerHTML = layer.tpl({
         name: 'john',
         arr:['apple','xiaomi','oppo']
