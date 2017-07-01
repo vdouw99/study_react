38657dcffa5c3623cf0b35ce7ec5ea0426fdaef0
zhangsanfeng <123456@qq.com>
2017-07-01 11:40:23 +0800

    引入js和css

--- me/project/src/index.jsx
+++ me/project/src/index.jsx
@@ -4,14 +4,23 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import './js.js';
+
+import './script/main.js';
+import './style/common.css';
+import './style/flex.css';
 
 // 定义组件
 class Hello extends React.Component {
     // 定义的一个函数
     render() {
         return (
-            <p>hello world, jsx is normal.</p>
+            <div>
+                <p>hello world, jsx is normal.</p>
+                <div className="flex-div">
+                    <div className="left">left</div>
+                    <div className="right">right</div>
+                </div>
+            </div>
         )
     }
 }
--- me/project/src/js.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by sf on 2017/7/1.
- */
-
-console.log('测试import文件引入');
