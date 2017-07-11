a6d497f6a962d4879f7beb307e64cb83b334da09
zhangsanfeng <123456@qq.com>
2017-07-11 11:37:34 +0800

    Router => url params

--- study_router/demo/src/index.js
+++ study_router/demo/src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Router01BasicExample from './Router01BasicExample.js';
+import Router02Params from './Router02Params.js';
 
 ReactDOM.render(
     <div>
         <App />
+        <hr />
         <h1>Router Basic Example</h1>
         <Router01BasicExample />
+        <hr />
+        <h1>url Params</h1>
+        <Router02Params />
     </div>
     , document.getElementById('root')
 );
