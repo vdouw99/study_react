326bccaaddc9cd672845659d8739840b182d1e13
zhangsanfeng <123456@qq.com>
2017-07-11 14:16:20 +0800

    Custom-Link

--- study_router/demo/src/index.js
+++ study_router/demo/src/index.js
@@ -6,6 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import Router01BasicExample from './Router01BasicExample.js';
 import Router02Params from './Router02Params.js';
 import Router03Auth from './Router03Auth.js';
+import Router04CustomLink from './Router04CustomLink.js';
 
 ReactDOM.render(
     <div>
@@ -19,6 +20,9 @@ ReactDOM.render(
         <hr />
         <h1>auth-workflow</h1>
         <Router03Auth />
+        <hr />
+        <h1>custom-link</h1>
+        <Router04CustomLink />
     </div>
     , document.getElementById('root')
 );
