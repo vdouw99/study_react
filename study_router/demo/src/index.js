2cef1aa396988a0526f294c7ca6c59dea732584f
zhangsanfeng <123456@qq.com>
2017-07-11 10:12:21 +0800

    BasicExample by router basic setting

--- study_router/demo/src/index.js
+++ study_router/demo/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Router01BasicExample from './Router01BasicExample.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <App />
+        <h1>Router Basic Example</h1>
+        <Router01BasicExample />
+    </div>
+    , document.getElementById('root')
+);
 registerServiceWorker();
