bfccec8ad0b9d71acbe8e114f2ef12d7d6e0c8a1
zhangsanfeng <123456@qq.com>
2017-07-13 15:48:39 +0800

    引入Switch，方能正常使用NotFound(404)

--- me/project/src/router/routeMap.jsx
+++ me/project/src/router/routeMap.jsx
@@ -4,11 +4,12 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
 
 import App from '../containers/App.jsx';
 import Home from '../containers/Home/index.jsx';
 import List from '../containers/List/index.jsx';
+import NotFound from '../containers/NotFound/index.jsx';
 
 class RouterMap extends React.Component {
     render() {
@@ -19,11 +20,15 @@ class RouterMap extends React.Component {
                         <li><Link to="/">App</Link></li>
                         <li><Link to="/home">Home</Link></li>
                         <li><Link to="/list">List</Link></li>
+                        <li><Link to="/notFound">NotFound</Link></li>
                     </ul>
                     <div style={{clear: 'both'}}></div>
-                    <Route exact path="/" component={App}/>
-                    <Route path="/home" component={Home}/>
-                    <Route path="/list" component={List}/>
+                    <Switch>
+                        <Route exact path="/" component={App}/>
+                        <Route path="/home" component={Home}/>
+                        <Route path="/list" component={List}/>
+                        <Route path="*" component={NotFound}/>
+                    </Switch>
                 </div>
             </Router>
         )
