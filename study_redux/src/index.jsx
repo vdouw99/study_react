05ea0dc48b0088feb4c29f904a0d714b2bb3ca5e
zhangsanfeng <123456@qq.com>
2017-07-17 10:22:41 +0800

    测试上传

--- study_redux/src/index.jsx
+++ study_redux/src/index.jsx
@@ -6,10 +6,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './app.js';
 
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
+import Redux02 from './Redux02.jsx';
+
 
 render(
     <div>
         <App />
+        <Provider>
+            <Redux02 />
+        </Provider>
     </div>,
     document.getElementById('root')
 );
