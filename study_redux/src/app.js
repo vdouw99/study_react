53cb5fd05f0589f8c7efc1d0ffb4d22b261b9e4f
zhangsanfeng <123456@qq.com>
2017-07-17 08:28:24 +0800

    更新一下原来的代码

--- study_redux/src/app.js
+++ study_redux/src/app.js
@@ -6,6 +6,21 @@ import React from 'react';
 import {render} from 'react-dom';
 import ReduxStore from './ReduxStore01.jsx';
 
+import {createStore} from 'redux';
+
+// const store = createStore(fn);
+
+const ADD_TODO = '添加Todo';
+function addTodo(text) {
+    return {
+        type: ADD_TODO,
+        text: text
+    }
+}
+
+const action = addTodo('learn Redux');
+document.write(JSON.stringify(action));
+
 class App extends React.Component {
     render() {
         return (
