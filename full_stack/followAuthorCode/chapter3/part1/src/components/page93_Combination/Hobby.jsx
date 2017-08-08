a0411cf089048f4aacbe76df52860ef95d77c5ef
zhangsanfeng <123456@qq.com>
2017-08-09 05:24:24 +0800

    无状态函数式组件 只需要根据输入生成组件。

--- full_stack/followAuthorCode/chapter3/part1/src/components/page93_Combination/Hobby.jsx
+++ full_stack/followAuthorCode/chapter3/part1/src/components/page93_Combination/Hobby.jsx
@@ -9,14 +9,22 @@ const propTypes = {
     hobby: PropTypes.string.isRequired
 };
 
-class Hobby extends React.Component {
-    render() {
-        return (
-            <li>
-                {this.props.hobby}
-            </li>
-        )
-    }
+// class Hobby extends React.Component {
+//     render() {
+//         return (
+//             <li>
+//                 {this.props.hobby}
+//             </li>
+//         )
+//     }
+// }
+
+// 无状态函数式组件
+// 没有内部state，不需要组件生命周期函数，只需要根据输入生成组件。
+function Hobby(props) {
+    return (
+        <li>{props.hobby}</li>
+    )
 }
 
 Hobby.propTypes = propTypes;
