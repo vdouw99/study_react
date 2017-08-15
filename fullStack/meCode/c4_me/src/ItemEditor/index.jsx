2f852e49588a698a2a7ebaee30513966775fd857
张三丰 <zhangyz@gveoe.cn>
2017-08-15 09:39:05 +0800

    删除node_modules

--- fullStack/meCode/c4_me/src/ItemEditor/index.jsx
+++ fullStack/meCode/c4_me/src/ItemEditor/index.jsx
@@ -8,13 +8,13 @@ class ItemEditor extends React.Component {
     render() {
         return (
             <div className="col-md-6 item-editor-component">
+                <div className="edit-area">
+                    <p><input placeholder="请填写标题"/></p>
+                    <textarea placeholder="请填写内容"/>
+                </div>
                 <div className="control-area">
                     <button className="btn btn-success">saveText</button>
                     <button className="btn secondary">取消</button>
-                </div>
-                <div className="edit-area">
-                    <input placeholder="请填写标题"/>
-                    <textarea placeholder="请填写内容"/>
                 </div>
             </div>
         )
