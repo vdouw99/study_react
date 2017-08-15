f55248f9988428e3536524263eab8a535b1866d7
张三丰 <zhangyz@gveoe.cn>
2017-08-15 17:40:34 +0800

    second time，I want to fly in the sky!!!

---
+++ fullStack/meCode/c4/src/components/Deskmark/index.jsx
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/8/15 0015.
+ */
+
+import React from 'react';
+import './index.less';
+
+class Deskmark extends React.Component{
+    render(){
+        return(
+            <div>Deskmark</div>
+        );
+    }
+}
+
+export default Deskmark;
---
+++ fullStack/meCode/c4/src/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/8/15 0015.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import Deskmark from '../src/components/Deskmark/index.jsx';
+
+render(
+    <div>
+        <Deskmark />
+    </div>,
+    document.getElementById('root')
+);
