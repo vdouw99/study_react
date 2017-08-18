bf76ad45a19633cf0bfedf1c95d27763140b105f
张三丰 <123456@123456.com>
2017-08-18 18:27:39 +0800

    复制一个，准备再次再次重新编写

---
+++ fullStack/meCode/c4/src/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2017/8/15 0015.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+
+
+
+render(
+    <div>
+        1234
+    </div>,
+    document.getElementById('root')
+);
