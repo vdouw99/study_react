207ede55fbc8191299d97ad97d22a54093f05185
zhangsanfeng <123456@qq.com>
2017-08-29 22:34:43 +0800

    新建一个项目，跟着imooc学Redux

---
+++ redux/imooc/src/index.jsx
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/8/15 0015.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+
+render(
+    <div>123</div>,
+    document.getElementById('root')
+);
