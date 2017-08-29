947c9f54735ac708d80122ea5972cbefdfb44af3
zhangsanfeng <123456@qq.com>
2017-08-30 00:04:06 +0800

    更新一下下，睡觉

--- redux/imooc/README.md
+++ redux/imooc/README.md
@@ -13,9 +13,12 @@ npm install react-redux redux
 第1步：将用户的行为，在actions/index.jsx里面先描述出来
     分别是“添加”、“过滤”、“切换”
 
+第2步：写响应，就是reducer/index.jsx
 
 Object.assign({id:1},{id:2},{id:3})
 把对象2和对象3，合并到对象1
+
+该学http://www.imooc.com/video/13214
 
 
 
---
+++ redux/imooc/src/reducers/index.jsx
@@ -0,0 +1,43 @@
+/**
+ * Created by sf on 2017/8/29.
+ */
+
+// reducer
+// 传入旧状态和action
+// 返回新状态
+
+// 每条事项的响应
+const todo = (state, action)=> {
+    switch (action.type) {
+        // 根据传入的action和旧state，输出新的state
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
+        case 'TOGGLE_TODO':
+            if (state.id !== action.id) {
+                return state;
+            }
+            return Object.assign({}, state, {
+                completed: !state.completed
+            });
+        default:
+            return state;
+    }
+};
+
+// 全部事项的响应
+const todos = (state, action)=> {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, todo(undefined, action)];
+        case 'TOGGLE_TODO':
+            return state.map(t=>todo(t, action));
+        default:
+            return state;
+    }
+};
+
+
