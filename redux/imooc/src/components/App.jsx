a972c869a646935099c0d1d74c84f6a96620bd77
zhangsanfeng <123456@qq.com>
2017-08-30 23:58:38 +0800

    第3步：编写组件（component组件、container组件）

--- redux/imooc/src/components/App.jsx
+++ redux/imooc/src/components/App.jsx
@@ -3,17 +3,28 @@
  */
 
 import React from 'react';
+
 import AddTodo from "../containers/AddTodo.jsx";
 import VisibleTodoList from "../containers/VisibleTodoList.jsx";
 import Footer from './Footer.jsx';
 
-const App = () =>(
-    <div>
-        <AddTodo />
-        <VisibleTodoList />
-        <Footer />
-    </div>
-);
+var App = function () {
+    return (
+        <div>
+            <AddTodo />
+            <VisibleTodoList />
+            <Footer />
+        </div>
+    );
+};
+
+// const App = () =>(
+//     <div>
+//         <AddTodo />
+//         <VisibleTodoList />
+//         <Footer />
+//     </div>
+// );
 
 export default App;
 
