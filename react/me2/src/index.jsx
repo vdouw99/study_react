498caf0922295c2dd7a78d908af09149f45d5b50
zhangsanfeng <123456@qq.com>
2017-09-04 00:01:37 +0800

    睡觉前临时更新一下

---
+++ react/me2/src/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * Created by sf on 2017/6/29.
+ */
+
+import React from 'react';
+import {render} from 'react-dom';
+
+
+render(
+    <div>
+        123
+    </div>,
+    document.getElementById('root')
+);
