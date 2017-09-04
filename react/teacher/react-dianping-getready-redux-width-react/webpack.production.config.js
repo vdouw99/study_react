b70499efa3f619e7c530b30abbbc0b6f2e32e2a1
zhangsanfeng <123456@qq.com>
2017-09-04 17:02:42 +0800

    验证老师的代码，基本OK（后期更新我的webpack配置，有很大的参考性）

--- react/teacher/react-dianping-getready-redux-width-react/webpack.production.config.js
+++ react/teacher/react-dianping-getready-redux-width-react/webpack.production.config.js
@@ -43,24 +43,24 @@ module.exports = {
 
     // 定义为生产环境，编译 React 时压缩到最小
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
+      // 'process.env':{
+      //   'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      // }
     }),
 
     // 为组件分配ID，通过这个插件webpack可以分析和优先考虑使用最多的模块，并为它们分配最小的ID
     new webpack.optimize.OccurenceOrderPlugin(),
-    
+
     new webpack.optimize.UglifyJsPlugin({
         compress: {
           //supresses warnings, usually from module minification
           warnings: false
         }
     }),
-    
+
     // 分离CSS和JS文件
-    new ExtractTextPlugin('/css/[name].[chunkhash:8].css'), 
-    
+    new ExtractTextPlugin('/css/[name].[chunkhash:8].css'),
+
     // 提供公共代码
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor',
@@ -68,8 +68,8 @@ module.exports = {
     }),
 
     // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
-    })
+    // new webpack.DefinePlugin({
+    //   __DEV__: false
+    // })
   ]
 }
