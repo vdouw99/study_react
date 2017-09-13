a4d5c8125a483396f3e9b4e68e16ed47f4d77214
zhangsanfeng <123456@qq.com>
2017-09-13 11:25:24 +0800

    通过Redux更新store，嗯，可以这么理解

--- react/me3/src/containers/City/index.jsx
+++ react/me3/src/containers/City/index.jsx
@@ -6,9 +6,15 @@ import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {hashHistory} from 'react-router';
 
 import Header from '../../components/Header/index.jsx';
 import CurrentCity from '../../components/CurrentCity/index.jsx';
+import CityList from '../../components/CityList/index.jsx';
+
+import {CITYNAME} from '../../config/index.jsx';
+import Util from '../../util/index.jsx';
+import * as userInfoActionsFromOtherFile from '../../actions/index.jsx'
 
 class Index extends React.Component {
     constructor(props, context) {
@@ -21,8 +27,21 @@ class Index extends React.Component {
             <div>
                 <Header title="选择城市"/>
                 <CurrentCity cityName={this.props.userinfo.cityName}/>
+                <CityList changeFn={this.changeCity.bind(this)}/>
             </div>
         );
+    }
+
+    changeCity(newCity) {
+        alert(newCity);
+        if (newCity == null) return false;
+        // 修改redux
+        const userinfo = this.props.userinfo;
+        userinfo.cityName = newCity;
+        this.props.userinfoActions.update(userinfo);
+        // 修改cookie
+        Util.setLocalStorage(CITYNAME, newCity);
+        hashHistory.push('/city');
     }
 
     componentDidMount() {
@@ -38,7 +57,9 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return {};
+    return {
+        userinfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch)
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Index);
