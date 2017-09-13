a948c08732ce385e5a63d1d68ae529e943e2c59f
zhangsanfeng <123456@qq.com>
2017-09-13 10:01:58 +0800

    跳转链接

--- react/me3/src/components/HomeHeader/index.jsx
+++ react/me3/src/components/HomeHeader/index.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import {Link} from 'react-router';
 
 import './index.less';
 
@@ -17,8 +18,10 @@ class Index extends React.Component {
         return (
             <div id="home-header" className="clear-fix">
                 <div className="home-header-left float-left">
-                    <span>{this.props.cityName}</span>
-                    <i className="icon-angle-down ml-3"/>
+                    <Link to="/city">
+                        <span>{this.props.cityName}</span>
+                        <i className="icon-angle-down ml-3"/>
+                    </Link>
                 </div>
                 <div className="home-header-right float-right">
                     <i className="icon-user"/>
